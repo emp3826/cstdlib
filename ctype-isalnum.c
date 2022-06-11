58a126f52e05e2bf4904bbe6f005e924b6726143
emp3826 <burpsuiteyyds@protonmail.com>
2022-06-11 17:45:47 +0800

    islower

--- ctype-isalnum.c
+++ ctype-isalnum.c
@@ -6,8 +6,7 @@ int main()
    int var1 = 'd';
    int var2 = '2';
    int var3 = '\t';
-   int var4 = ' ';
-    
+   int var4 = ' '; 
    isalnum(var1) ? printf("%cis an alnum\n", var1) : printf("%cis not an alnum\n", var1);
    isalnum(var2) ? printf("%cis an alnum\n", var2) : printf("%cis not an alnum\n", var2);
    isalnum(var3) ? printf("%cis an alnum\n", var3) : printf("%cis not an alnum\n", var3);
--- ctype-isalpha.c
+++ ctype-isalpha.c
@@ -7,7 +7,6 @@ int main()
    int var2 = '2';
    int var3 = '\t';
    int var4 = ' ';
-    
    isalpha(var1) ? printf("%cis an alpha\n", var1) : printf("%cis not an alpha\n", var1);
    isalpha(var2) ? printf("%cis an alpha\n", var2) : printf("%cis not an alpha\n", var2);
    isalpha(var3) ? printf("%cis an alpha\n", var3) : printf("%cis not an alpha\n", var3);

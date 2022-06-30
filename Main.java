0fc8c80943e32c3e16ab637ff16469d2712d9bbb
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-07-01 01:35:46 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,33 @@
+public class Main{
+	public static void main(String[] args){
+		Person ming = new Person();
+		ming.setName("xiaoming");
+		ming.setAge(12);
+		System.out.println(ming.getName());
+		System.out.println(ming.getAge());
+	}
+}
+
+class Person{
+	private String name;
+	private int age;
+
+	
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+
+	public void setAge(int age){
+		if(age < 0){
+			throw new IllegalArgumentException("invalid argument");
+		}
+		this.age = age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+}

68e42c9d3876c213acad9cd3b3ac2fbcbb98fce6
emp3826 <30045531+emp3826@users.noreply.github.com>
2022-08-17 01:14:02 +0800

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -8,12 +8,20 @@ void increment2(int *p)
 {
 	*p = *p + 1;
 }
+int increment3(int p)
+{
+	p++;
+	return p;
+}
 int main()
 {
 	int a = 1;
 	int b = 1;
+	int c = 1;
 	increment1(a);
 	increment2(&b);
+	c = increment(c);
 	printf("%d\n",a);
 	printf("%d\n",b);
+	printf("%d\n",c);
 }
